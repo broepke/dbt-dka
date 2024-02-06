34fd01cd9ace3cc94aba7f19b3e6bdb2da413329
Brian Roepke <brian@dataknowsall.com>
2024-02-06 14:43:45 +0000

    moved the total column earlier

--- models/marts/deadpool/score_twenty_four.sql
+++ models/marts/deadpool/score_twenty_four.sql
@@ -17,20 +17,20 @@ select
     b.player,
     b.email,
     b.year,
-    b.score as base_score,
-    to_decimal(coalesce(f.score, 0)) as first_blood,
-    to_decimal(coalesce(l.score, 0)) as last_blood,
-    to_decimal(coalesce(qo.points, 0)) as q_one,
-    to_decimal(coalesce(qtw.points, 0)) as q_two,
-    to_decimal(coalesce(qth.points, 0)) as q_three,
-    to_decimal(coalesce(qf.points, 0)) as q_four,
     to_decimal(b.score)
     + to_decimal(coalesce(f.score, 0))
     + to_decimal(coalesce(l.score, 0))
     + to_decimal(coalesce(qo.points, 0))
     + to_decimal(coalesce(qtw.points, 0))
     + to_decimal(coalesce(qth.points, 0))
-    + to_decimal(coalesce(qf.points, 0)) as score
+    + to_decimal(coalesce(qf.points, 0)) as total,
+    b.score as base_score,
+    to_decimal(coalesce(f.score, 0)) as first_blood,
+    to_decimal(coalesce(l.score, 0)) as last_blood,
+    to_decimal(coalesce(qo.points, 0)) as q_one,
+    to_decimal(coalesce(qtw.points, 0)) as q_two,
+    to_decimal(coalesce(qth.points, 0)) as q_three,
+    to_decimal(coalesce(qf.points, 0)) as q_four
 from base_scoring b
 left join first_blood f on b.id = f.id
 left join last_blood l on b.id = l.id
@@ -39,4 +39,4 @@ left join q_two qtw on b.id = qtw.id
 left join q_three qth on b.id = qth.id
 left join q_four qf on b.id = qf.id
 where year = 2024
-order by score desc
+order by total desc
