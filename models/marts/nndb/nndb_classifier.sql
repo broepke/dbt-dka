84ff79bca4fdb5e7006706923c660279e7204c0e
Brian Roepke <brian@dataknowsall.com>
2024-04-23 16:07:25 +0000

    Added first draft of OHE columns for risk factors

--- models/marts/nndb/nndb_classifier.sql
+++ models/marts/nndb/nndb_classifier.sql
@@ -1,14 +1,17 @@
-with source as (select * from {{ ref("stg_nndb__email_nndb") }})
+with source as (select * from {{ ref("stg_nndb__email_nndb") }}),
+
+flattened as ( select * from {{ ref('int_nndb__flattened_risks') }}) 
 
 select
-    id,
-    gender,
-    race,
-    occupation,
-    nationality,
-    -- birth_date,
-    age,
-    risk_factors,
-    wiki_categorized_cause,
-    case when death_date is null then 0 else 1 end as is_dead
-from source
+    s.id,
+    s.gender,
+    s.race,
+    s.occupation,
+    s.nationality,
+    s.age,
+    s.wiki_categorized_cause,
+    f.* exclude id,
+    case when s.death_date is null then 0 else 1 end as is_dead
+from source s
+left join flattened f 
+on f.id = s.id
