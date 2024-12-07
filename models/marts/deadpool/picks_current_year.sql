6089434fa15670b9ea8530b1f666f3ccae57bd57
Brian Roepke <brian@dataknowsall.com>
2024-12-07 19:16:38 +0000

    Added wiki fields

--- models/marts/deadpool/picks_current_year.sql
+++ models/marts/deadpool/picks_current_year.sql
@@ -14,6 +14,8 @@ transform as (
         pe.birth_date,
         pe.death_date,
         pe.age,
+        pe.wiki_page,
+        pe.wiki_id,
         concat(pl.first_name || ' ' || pl.last_name) as player,
         pe.timestamp as draft_date
     from {{ ref('stg_deadpool__players') }} pl
