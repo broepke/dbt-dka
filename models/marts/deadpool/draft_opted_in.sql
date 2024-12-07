f7eba54e99c82739957862f651c6e13c202daf76
Brian Roepke <brian@dataknowsall.com>
2024-12-07 17:45:20 +0000

    Removed Reference to YEAR_TWO - now properly pulling from the DRAFT_ORDER Table

--- models/marts/deadpool/draft_opted_in.sql
+++ models/marts/deadpool/draft_opted_in.sql
@@ -4,4 +4,8 @@ select
     pl.email, 
     pl.sms
 from {{ ref('stg_deadpool__players') }} pl
-where pl.year_two is not null and pl.opt_in = true
+join {{ ref('stg_deadpool__draft_order') }} do
+on pl.id = do.player_id
+where do.year = year(current_date)
+and do.draft_order is not null
+and pl.opt_in = true
