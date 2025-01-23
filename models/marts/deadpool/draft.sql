1a85641db938e36332e9b554cee5ccb4f8295bf7
Brian Roepke <brian@dataknowsall.com>
2025-01-23 19:07:43 +0000

    Added a year filter to the draft

--- models/marts/deadpool/draft.sql
+++ models/marts/deadpool/draft.sql
@@ -12,5 +12,6 @@ left join
     {{ ref("stg_deadpool__draft_order") }} do on pl.id = do.player_id and do.year = year(current_date)
 where do.draft_order is not null
 and pe.death_date is null
+and do.year = year(current_date)
 group by 1, 2, 3
-order by 2, 3 desc
+order by 1, 2, 3 desc
